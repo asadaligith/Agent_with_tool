0f71a79f5f5375399df47bb7e708a954d4449b50
asadaligith <asad2747982@gmail.com>
2025-08-03 11:15:28 +0500

    Agent with tool

---
+++ src/tools/main.py
@@ -0,0 +1,39 @@
+from agents import Agent , Runner , OpenAIChatCompletionsModel , set_tracing_disabled 
+from openai import AsyncOpenAI 
+from dotenv import load_dotenv
+import asyncio
+import os
+
+load_dotenv()
+set_tracing_disabled(True)
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+Model = OpenAIChatCompletionsModel(
+    model= "gemini-2.5-flash",
+    openai_client=external_client
+)
+
+Frontend_agent = Agent(
+    name="Frontend Agent",
+    instructions=
+    """
+    You are expert in Frontend you give answer to user about frontend related query
+    """,
+    model=Model
+)
+
+
+async def main():   
+    response = await Runner.run(
+        Frontend_agent,
+        input="what is Frontend",
+    )
+    print(response.final_output)
+
+def start():
+    asyncio.run(main())
